d57fc29711ff7982a157a25562276b2ed35ed672
Cathryn Li <Cathryn@MacBook-Pro.local>
2020-01-31 13:23:59 -0500

    Initial commit

---
+++ app/src/main/java/com/licathryn/firstapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.licathryn.firstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+// class number = 8
+// cathrynli606@gmail.com
